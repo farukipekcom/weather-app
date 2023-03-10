5e0136c0accae671dc9e2ae05d1b91c5e79a7065
Faruk İpek <farukipekweb@gmail.com>
2023-03-09 19:01:09 -0500

    fix config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-/** @type {import('next').NextConfig} */
+const path = require("path");
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
-  }, nextConfig
-}
+  },
+};
